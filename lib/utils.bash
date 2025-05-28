004b9b9ab590043fc991299267fc180cdd468ca2
Tom Duckering <tom.duckering@gmail.com>
2025-05-28 22:37:07 +0100

    Another attempt to fix download and install scripts

--- lib/utils.bash
+++ lib/utils.bash
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for pkl-aws-secret-resource-reader.
-GH_REPO="https://github.com/infrablocks/pkl-aws-secret-resource-reader"
-TOOL_NAME="pkl-aws-secret-resource-reader"
-TOOL_TEST="pkl-aws-secret-resource-reader --help"
+GH_REPO="https://github.com/infrablocks/pkl_aws_secret_resource_reader"
+TOOL_NAME="pkl_aws_secret_resource_reader"
+TOOL_TEST="pkl_aws_secret_resource_reader --help"
 
 fail() {
 	echo -e "asdf-$TOOL_NAME: $*"
@@ -14,7 +14,7 @@ fail() {
 
 curl_opts=(-fsSL)
 
-# NOTE: You might want to remove this if pkl-aws-secret-resource-reader is not hosted on GitHub releases.
+# NOTE: You might want to remove this if pkl_aws_secret_resource_reader is not hosted on GitHub releases.
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
 	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
@@ -27,7 +27,7 @@ sort_versions() {
 list_github_tags() {
 	git ls-remote --tags --refs "$GH_REPO" |
 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+		sed 's/^v//' | grep -v 0.0.0
 }
 
 list_all_versions() {
@@ -48,27 +48,27 @@ download_release() {
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
-install_version() {
-	local install_type="$1"
-	local version="$2"
-	local install_path="${3%/bin}/bin"
+# install_version() {
+# 	local install_type="$1"
+# 	local version="$2"
+# 	local install_path="${3%/bin}/bin"
 
-	if [ "$install_type" != "version" ]; then
-		fail "asdf-$TOOL_NAME supports release installs only"
-	fi
+# 	if [ "$install_type" != "version" ]; then
+# 		fail "asdf-$TOOL_NAME supports release installs only"
+# 	fi
 
-	(
-		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+# 	(
+# 		mkdir -p "$install_path"
+# 		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-		# TODO: Assert pkl-aws-secret-resource-reader executable exists.
-		local tool_cmd
-		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
-		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+# 		# TODO: Assert pkl-aws-secret-resource-reader executable exists.
+# 		local tool_cmd
+# 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+# 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
-		echo "$TOOL_NAME $version installation was successful!"
-	) || (
-		rm -rf "$install_path"
-		fail "An error occurred while installing $TOOL_NAME $version."
-	)
-}
+# 		echo "$TOOL_NAME $version installation was successful!"
+# 	) || (
+# 		rm -rf "$install_path"
+# 		fail "An error occurred while installing $TOOL_NAME $version."
+# 	)
+# }
